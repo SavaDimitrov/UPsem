4bdd158b6ce447020130708e088c5fee0e7a9c41
SavaDimitrov <savad.9310@gmail.com>
2016-11-11 12:56:31 +0200

    Add files via upload

---
+++ semWeek6/semWeek6/Source.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		//First problem.
+	//int arr[10];
+	//int count = 0;
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cin >> arr[i];
+	//	if (arr[i] > 5)
+	//	{
+	//		count++;
+	//	}
+	//}
+	//cout << count << endl;
+	
+		//Second problem
+	//int arr[10];
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cin >> arr[i];
+	//	system("cls");
+	//}
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << "arr[" << i << "]= " << arr[i] << "; ";
+	//	if (i == 4) cout << endl;
+	//}
+	//cout << endl;
+
+		//Third problem
+	//int arr[15];
+	//bool check = false; //dali raste
+	//bool check1 = false; //dali namalqva
+
+	//for (int i = 0; i < 15; i++)
+	//{
+	//	cin >> arr[i];
+	//}
+	//for (int i = 0; i < 14; i++)
+	//{
+	//	if (arr[i] < arr[i + 1])
+	//	{
+	//		check = true;
+	//	}
+	//	if (arr[i] > arr[i + 1])
+	//	{
+	//		check1 = true;
+	//	}
+	//}
+	//
+	//if (check && check1) cout << "Nito raste, nito namalqva" << endl;
+	//else if (check) cout << "Raste!";
+	//else if (check1) cout << "Namalqva!";
+	//else cout << "Constanten!" << endl;
+
+
+		//Fourth problem
+	// intarr[kolkoto tolkova]; da oburnem masiva v obraten red;
+
+
+	system("pause");
+	return 0;
+}
