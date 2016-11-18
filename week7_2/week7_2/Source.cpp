92a50fd55fcf474f23b82a07167fa333095a4137
SavaDimitrov <savad.9310@gmail.com>
2016-11-18 12:58:48 +0200

    Add files via upload

---
+++ week7_2/week7_2/Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		//umnojenie na matrici
+
+	//int mat1[3][2], mat2[2][5], mat3[3][5] = { 0 };
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for (int j = 0; j < 2; j++)
+	//	{
+	//		cin >> mat1[i][j];
+	//	}
+	//}
+
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	for (int j = 0; j < 5; j++)
+	//	{
+	//		cin >> mat2[i][j];
+	//	}
+	//}
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for (int j = 0; j < 5; j++)
+	//	{
+	//		for (int k = 0; k < 2; k++)
+	//		{
+	//			mat3[i][j] += mat1[i][k] * mat2[k][j];
+	//		}
+	//	}
+	//}
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for (int j = 0; j < 5; j++)
+	//	{
+	//		cout << mat3[i][j] << " ";
+	//	}
+	//	cout << endl;
+	//}
+
+
+		 //functions
+	/*
+	тип име (променливи(имат типове и т.н.) //int, bool, float, void
+						 // ако е void няма return!	
+	{
+		прави нещо;
+		return нещо;
+	}
+	*/
+
+
+
+	system("pause");
+	return 0;
+
+}
